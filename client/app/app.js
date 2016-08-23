68abcbbe2313eaffe1a167109b0995daf2af733d
Hoonio <hoonio@hoonio.com>
2016-08-23 02:07:48 +0100

    move cdn ref to local

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,7 @@
 'use strict';
 
 angular.module('gadriApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
   'ui.router',
-  'ui.bootstrap',
   'ngMap'
 ])
   .constant('DATA_FEED', 'https://spreadsheets.google.com/feeds/list/1rnHdgtxBbGIRFT1Gz1cjhKi8An366SUUF2ArAY16uGA/1/public/values?gid=87971418&alt=json')
